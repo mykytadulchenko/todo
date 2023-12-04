492a7f421f79500306716aaec7484ff2869bbd87
Mykyta Dulchenko <mykyta.dulchenko@devico.io>
2023-12-04 14:52:35 +0200

    Minor improvement: Edit item option implemented

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 class Todo {
     constructor(root) {
         this.root = root
-        this.tasks = []
     }
 
     init() {
@@ -20,8 +19,13 @@ class Todo {
     renderControls() {
         this.controls = document.createElement('form')
         this.controls.classList.add('controls')
-        this.controls.append(document.createElement('input'))
+        const input = document.createElement('input')
+        input.placeholder = 'Input task...'
+        this.controls.append(input)
         this.controls.addEventListener('submit', (e) => {
+            e.preventDefault()
+            if(!input.value) return
+            this.addItem(input.value)
         })
         this.root.append(this.controls)
     }
@@ -29,7 +33,11 @@ class Todo {
     renderItem(data) {
         const item = document.createElement('li')
         item.classList.add('list__item')
-        item.textContent = data.title
+        item.textContent = data.value
+        item.addEventListener('dblclick', () => {
+            item.innerHTML = ''
+            this.editItem(item, data)
+        })
         return item
     }
 
@@ -46,6 +54,37 @@ class Todo {
 
     setTasks(data) {
         localStorage.setItem('todos', JSON.stringify(data))
+    }
+
+    addItem(value) {
+        const data = this.getTasks()
+        data.push({id: data.at(-1)?.id + 1 || 0, value: value, isFinished: false})
+        this.setTasks(data)
+        this.renderList()
+    }
+
+    editItem(domElement, data) {
+        const editForm = document.createElement('form')
+        const input = document.createElement('input')
+        input.value = data.value
+        editForm.append(input)
+        const onSuccess = () => {
+            const filteredData = this.getTasks().filter(item => item.id !== data.id)
+            this.setTasks(filteredData)
+            this.addItem(input.value)
+        }
+        editForm.addEventListener('submit', (e) => {
+            e.preventDefault()
+            if(input.value && input.value !== data.value) onSuccess()
+            else this.renderList()
+        })
+        domElement.append(editForm) 
+        input.focus()
+    }
+
+    removeItem(data) {
+        const filteredData = this.getTasks().filter(item => item.id !== data.id)
+        this.setTasks(filteredData)
     }
 }
 
